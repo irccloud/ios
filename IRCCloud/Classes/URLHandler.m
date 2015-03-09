ad136e9164a821d94fd202e7616562edd9c645a9
Sam Steele <sam.steele@gmail.com>
2015-03-09 09:56:04 -0700

    The image viewer can only handle http(s) URLs

--- IRCCloud/Classes/URLHandler.m
+++ IRCCloud/Classes/URLHandler.m
@@ -54,7 +54,7 @@
 {
     NSString *l = [url.path lowercaseString];
     // Use pre-processor macros instead of variables so conditions are still evaluated lazily
-    return (HAS_IMAGE_SUFFIX(l) || IS_IMGUR(url) || IS_FLICKR(url) || IS_INSTAGRAM(url) || IS_DROPLR(url) || IS_CLOUDAPP(url) || IS_STEAM(url) || IS_LEET(url));
+    return ([url.scheme.lowercaseString isEqualToString:@"http"] || [url.scheme.lowercaseString isEqualToString:@"https"]) && (HAS_IMAGE_SUFFIX(l) || IS_IMGUR(url) || IS_FLICKR(url) || IS_INSTAGRAM(url) || IS_DROPLR(url) || IS_CLOUDAPP(url) || IS_STEAM(url) || IS_LEET(url));
 }
 
 - (void)launchURL:(NSURL *)url
