2ac58a7474609a2293fd332a7b4117e8ee307da2
Sam Steele <sam.steele@gmail.com>
2016-08-17 07:57:41 -0400

    Fix channels not being marked as read when new messages arrive

--- IRCCloud/Classes/EventsTableView.m
+++ IRCCloud/Classes/EventsTableView.m
@@ -1065,6 +1065,7 @@ float __largeAvatarHeight = 32;
             if(!_buffer.scrolledUp) {
                 [self scrollToBottom];
                 [self _scrollToBottom];
+                [self sendHeartbeat];
             } else if(!event.isSelf && [event isImportant:_buffer.type]) {
                 _newMsgs++;
                 if(event.isHighlight)
