5517535ba02007d10087d878b1141614b8c47036
Sam Steele <sam.steele@gmail.com>
2017-03-09 13:59:25 -0500

    Fix prefs parsing when the string contains emoji

--- IRCCloud/Classes/NetworkConnection.m
+++ IRCCloud/Classes/NetworkConnection.m
@@ -1808,7 +1808,7 @@ static void ReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReach
     @synchronized(self) {
         if(!_prefs && self.userInfo && [[self.userInfo objectForKey:@"prefs"] isKindOfClass:[NSString class]] && [[self.userInfo objectForKey:@"prefs"] length]) {
             NSError *error;
-            _prefs = [NSJSONSerialization JSONObjectWithData:[NSData dataWithBytes:[[self.userInfo objectForKey:@"prefs"] UTF8String] length:[[self.userInfo objectForKey:@"prefs"] length]] options:kNilOptions error:&error];
+            _prefs = [NSJSONSerialization JSONObjectWithData:[[self.userInfo objectForKey:@"prefs"] dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&error];
             if(error) {
                 CLS_LOG(@"Prefs parse error: %@", error);
                 CLS_LOG(@"JSON string: %@", [self.userInfo objectForKey:@"prefs"]);
