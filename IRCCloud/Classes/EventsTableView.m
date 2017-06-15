e0f7a11ef340a11fc06088765990c4b9b6e7888f
Sam Steele <sam.steele@gmail.com>
2017-06-15 10:27:33 -0400

    typo

--- IRCCloud/Classes/EventsTableView.m
+++ IRCCloud/Classes/EventsTableView.m
@@ -68,7 +68,7 @@ void WFSimulate3DTouchPreview(id<UIViewControllerPreviewing> previewer, CGPoint 
             [interactionController commitInteractivePreview];
             //[interactionController cancelInteractivePreview];
         });
-    });(/
+    });*/
 }
 #endif
 
