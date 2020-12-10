f780e73cfa5ad9f4724b0b724442a2cae815434e
Sam Steele <sam.steele@gmail.com>
2020-12-10 06:36:08 -0500

    Fix a crash

--- IRCCloud/Classes/URLHandler.m
+++ IRCCloud/Classes/URLHandler.m
@@ -112,7 +112,7 @@
 
 #define IS_GIPHY(url) ((([url.host.lowercaseString isEqualToString:@"giphy.com"] || [url.host.lowercaseString isEqualToString:@"www.giphy.com"]) && [url.path.lowercaseString hasPrefix:@"/gifs/"]) || [url.host.lowercaseString isEqualToString:@"gph.is"])
 
-#define IS_YOUTUBE(url) ((([url.host.lowercaseString isEqualToString:@"youtube.com"] || [url.host.lowercaseString hasSuffix:@".youtube.com"]) && [url.path.lowercaseString hasPrefix:@"/watch"]) || [url.host.lowercaseString isEqualToString:@"youtu.be"])
+#define IS_YOUTUBE(url) ((([url.host.lowercaseString isEqualToString:@"youtube.com"] || [url.host.lowercaseString hasSuffix:@".youtube.com"]) && [url.path.lowercaseString hasPrefix:@"/watch"]) || ([url.host.lowercaseString isEqualToString:@"youtu.be"] && url.path.length > 1))
 
 #define IS_WIKI(url) ([url.path.lowercaseString hasPrefix:@"/wiki/"] && HAS_IMAGE_SUFFIX(url.absoluteString.lowercaseString))
 
