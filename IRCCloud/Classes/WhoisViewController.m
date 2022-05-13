c94ef45554595dbadeb23565a6017507744330cf
Sam Steele <sam.steele@gmail.com>
2022-05-12 22:23:29 -0400

    Add some missing fields to whois dialog

--- IRCCloud/Classes/WhoisViewController.m
+++ IRCCloud/Classes/WhoisViewController.m
@@ -41,8 +41,19 @@
     return self;
 }
 
+-(void)appendWhoisLine:(NSMutableAttributedString *)data key:(NSString *)key data:(IRCCloudJSONObject *)object nick:(NSString *)nick server:(Server *)s {
+    [self appendWhoisLine:data key:key data:object nick:nick server:s format:@"%@ %@\n"];
+}
+
+-(void)appendWhoisLine:(NSMutableAttributedString *)data key:(NSString *)key data:(IRCCloudJSONObject *)object nick:(NSString *)nick server:(Server *)s format:(NSString *)fmt {
+    if([[object objectForKey:key] length]) {
+        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:fmt, nick, [object objectForKey:key]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
+    }
+}
+
 -(void)setData:(IRCCloudJSONObject *)object {
-    self.navigationItem.title = [object objectForKey:@"nick"];
+    NSString *nick = [object objectForKey:@"user_nick"] ? [object objectForKey:@"user_nick"] : [object objectForKey:@"nick"];
+    self.navigationItem.title = nick;
 
     Server *s = [[ServersDataSource sharedInstance] getServer:[[object objectForKey:@"cid"] intValue]];
     
@@ -79,26 +90,12 @@
         [data appendAttributedString:[[NSAttributedString alloc] initWithString:@"\n"]];
     }
     
-    if([[object objectForKey:@"op_nick"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"op_nick"], [object objectForKey:@"op_msg"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
-    if([[object objectForKey:@"opername"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"nick"], [object objectForKey:@"opername"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
-    if([[object objectForKey:@"userip"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"nick"], [object objectForKey:@"userip"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
-    if([[object objectForKey:@"bot_msg"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"nick"], [object objectForKey:@"bot_msg"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
-    if([[object objectForKey:@"server_addr"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ is connected via: %@", [object objectForKey:@"nick"], [object objectForKey:@"server_addr"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
+    [self appendWhoisLine:data key:@"bot_msg" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"op_msg" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"opername_msg" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"userip" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"server_addr" data:object nick:nick server:s format:@"%@ is connected via: %@"];
+   
     if([[object objectForKey:@"server_extra"] length]) {
         [data appendAttributedString:[ColorFormatter format:@" (" defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:nil links:nil]];
         NSUInteger offset = data.length;
@@ -121,10 +118,9 @@
     
     [data appendAttributedString:[ColorFormatter format:@"\n" defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:nil links:nil]];
     
-    if([[object objectForKey:@"host"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"nick"], [object objectForKey:@"host"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
+    [self appendWhoisLine:data key:@"host" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"country" data:object nick:nick server:s];
+
     [self addChannels:[object objectForKey:@"channels_oper"] forGroup:@"Oper" attributedString:data links:links server:s];
     [self addChannels:[object objectForKey:@"channels_owner"] forGroup:@"Owner" attributedString:data links:links server:s];
     [self addChannels:[object objectForKey:@"channels_admin"] forGroup:@"Admin" attributedString:data links:links server:s];
@@ -132,35 +128,24 @@
     [self addChannels:[object objectForKey:@"channels_halfop"] forGroup:@"Half-Operator" attributedString:data links:links server:s];
     [self addChannels:[object objectForKey:@"channels_voiced"] forGroup:@"Voiced" attributedString:data links:links server:s];
     [self addChannels:[object objectForKey:@"channels_member"] forGroup:@"Member" attributedString:data links:links server:s];
-    
-    if([[object objectForKey:@"secure"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"nick"], [object objectForKey:@"secure"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
-    if([[object objectForKey:@"client_cert"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"nick"], [object objectForKey:@"client_cert"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
-    if([[object objectForKey:@"cgi"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"nick"], [object objectForKey:@"cgi"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
-    if([[object objectForKey:@"help"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"nick"], [object objectForKey:@"help"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
-    if([[object objectForKey:@"vworld"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"nick"], [object objectForKey:@"vworld"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
-    if([[object objectForKey:@"modes"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"nick"], [object objectForKey:@"modes"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
-    if([[object objectForKey:@"stats_dline"] length]) {
-        [data appendAttributedString:[ColorFormatter format:[NSString stringWithFormat:@"%@ %@\n", [object objectForKey:@"nick"], [object objectForKey:@"stats_dline"]] defaultColor:[UIColor messageTextColor] mono:NO linkify:NO server:s links:nil]];
-    }
-    
+
+    [self appendWhoisLine:data key:@"secure" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"client_cert" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"cgi" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"help" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"staff" data:object nick:nick server:s format:@"%@ is staff: %@\n"];
+    [self appendWhoisLine:data key:@"modes" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"callerid" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"stats_dline" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"btn_metadata" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"text" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"msg_only_reg" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"suspend" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"chanop" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"kill" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"helper" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"admin" data:object nick:nick server:s];
+    [self appendWhoisLine:data key:@"codepage" data:object nick:nick server:s];
     
     self->_label.attributedText = data;
     self->_label.linkAttributes = [UIColor linkAttributes];
